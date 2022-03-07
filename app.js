92e2fb035590189915e124520e5099ed2718f0f5
Zachary Boelter <zboelter@gmail.com>
2022-03-07 16:37:50 -0600

    la fin

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+//event listener
 document.querySelector('#searchInput').addEventListener('keyup', function(e){
     let searchQuery = e.target.value.toLowerCase();
     let allNames = document.getElementsByClassName('name');
-
+//for loop iterating over all names
     for (let counter = 0; counter < allNames.length; counter++){
+        //set them to var
         const currentName = allNames[counter].textContent.toLowerCase();
         
+        //conditional checking the input against array of names. change style depending on match
         if (currentName.includes(searchQuery)) {
             allNames[counter].style.display = "block";
         } else {
