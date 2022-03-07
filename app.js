70546fd1b97478dfcd1339d66a1ce889c035f664
Zachary Boelter <zboelter@gmail.com>
2022-03-07 16:34:22 -0600

    working

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 document.querySelector('#searchInput').addEventListener('keyup', function(e){
     let searchQuery = e.target.value.toLowerCase();
-    console.log(searchQuery);
-})
+    let allNames = document.getElementsByClassName('name');
+
+    for (let counter = 0; counter < allNames.length; counter++){
+        const currentName = allNames[counter].textContent.toLowerCase();
+        
+        if (currentName.includes(searchQuery)) {
+            allNames[counter].style.display = "block";
+        } else {
+            allNames[counter].style.display = "none";
+        }
+    }
+    searchQuery.textContent = '';
+});
